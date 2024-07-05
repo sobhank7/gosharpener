0f6dad4b669571b5fc6cef01319f124174b8f3e6
sobhank7 <sobhanbabu.katikapalle@gmail.com>
2024-07-05 21:22:28 +0530

    first commit

---
+++ src/main/java/com/scaler/gosharpener_backend/GosharpenerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.gosharpener_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GosharpenerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GosharpenerBackendApplication.class, args);
+	}
+
+}
